dab70b92a73f7aeed8c0a56f41e303f9b972e1bb
ByrMucahit <m.mucahitbayar@gmail.com>
2022-07-06 13:19:34 +0300

    Added my project

---
+++ src/main/java/com/example/ownablebackenddevelopment/OwnableBackendDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ownablebackenddevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnableBackendDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnableBackendDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ownablebackenddevelopment/OwnableBackendDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ownablebackenddevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnableBackendDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
