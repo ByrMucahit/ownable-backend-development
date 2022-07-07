7941a551479f84c7a057b63f412221cab0cf620f
ByrMucahit <m.mucahitbayar@gmail.com>
2022-07-07 10:38:35 +0300

    Fixes the BUG #1. Finishes the web security side

--- src/main/java/com/example/ownablebackenddevelopment/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/example/ownablebackenddevelopment/security/jwt/AuthTokenFilter.java
@@ -3,9 +3,11 @@ package com.example.ownablebackenddevelopment.security.jwt;
 import com.example.ownablebackenddevelopment.security.services.UserDetailsServiceImp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.security.core.
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -28,8 +30,19 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         try
             {
                 String jwt = parseJwt(request);
-                if(jwt != null && jwtUtils)
-            }
+                if(jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                    String username = jwtUtils.getUserNameFromJwtToken(jwt);
+                    UserDetails userDetails = userDetailsServiceImp.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
     }
 
     private String parseJwt(HttpServletRequest request) {
